eb5f1a78ccc86839c45eaa8764432998de94dbf6
Jean <jeanwfsantos@gmail.com>
2018-11-11 21:15:26 -0200

    Add package react-google-maps, and component GoogleMap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-    <div>Projeto - Mapa do bairro</div>,
+import GoogleMap from './components/GoogleMap/index';
+
+render(
+    <div>
+        <h1>Projeto - Mapa do bairro</h1>
+        <GoogleMap
+            googleMapURL={
+                'https://maps.googleapis.com/maps/api/js?key=AIzaSyAe3FaSbC6tpAAnbQiE0HQX0nBuQBGEyWM&v=3.exp&libraries=geometry,drawing,places'
+            }
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+    </div>,
     document.getElementById('app')
 );
 
